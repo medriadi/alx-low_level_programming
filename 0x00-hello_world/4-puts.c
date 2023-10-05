9d4221aa33f02954c44a555bb1b52419dcaf43c1
medriadi <mohamedriadi@outlook.com>
2023-10-05 08:47:36 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Use the 'puts' function to print the desired message followed by a newline
+    puts("Programming is like building a multilingual puzzle\n");
+
+    // Return 0 to indicate successful program execution
+    return (0);
+}
