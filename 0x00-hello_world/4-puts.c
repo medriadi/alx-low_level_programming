67e2d4545d301786c6a6854f779819a7cea1ef51
medriadi <mohamedriadi@outlook.com>
2023-10-05 08:52:33 -0700

    Task 4 Fix Attempt 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    // Use the 'puts' function to print the desired message followed by a newline
+    /* Use the 'puts' function to print the desired message followed by a newline */
     puts("Programming is like building a multilingual puzzle\n");
 
-    // Return 0 to indicate successful program execution
+    /* Return 0 to indicate successful program execution */
     return (0);
 }
