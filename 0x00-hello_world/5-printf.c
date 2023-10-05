5ad9d180be9da13a9ae64432349c0896119b60ba
medriadi <mohamedriadi@outlook.com>
2023-10-05 09:10:20 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+* main - display "with proper grammar, but the outcome is a piece of art"
+* 
+* Return : 0 and exit the program (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
