08fd79d2bbb783a4ffec67b9796249e196251e9b
medriadi <mohamedriadi@outlook.com>
2023-10-05 09:47:11 -0700

    Task 8 FIX FIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Description: Prints the specified message to the standard error.
- * Return: Always 1 (Error).
+ * main - display "and that piece of art is useful" in console
+ * followed by a new line, to the standard error
+ * Return: 1 and exit the program
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
