e3ecf876bdf18849d77a168626c8b82cb803d4d1
medriadi <mohamedriadi@outlook.com>
2023-11-02 17:19:07 +0100

    Task 5 fix Attempt

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int is_positive_number(const char *str);
+char *multiply_strings(const char *num1, const char *num2);
+int main(int argc, char *argv[]);
 
 #endif
