261f6d5b1ac68cc92c5500636e9eea084edabe7b
medriadi <mohamedriadi@outlook.com>
2023-11-02 17:27:52 +0100

    Fix Attempt number 2 for Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,16 +14,62 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		_puts("Error");
+		_exit(98);
 	}
 	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
+		n1 = _atoi(argv[1]);
+		n2 = _atoi(argv[2]);
 		mul = n1 * n2;
-
-		printf("%d\n", mul);
+		_print(mul);
+		_putchar('\n');
 	}
 
-	return (0);
+	_exit(0);
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The input string.
+ * Return: The integer value.
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = n * 10 + (s[i] - '0');
+		}
+		else
+		{
+			_puts("Error");
+			_exit(98);
+		}
+		i++;
+	}
+
+	return (n);
+}
+
+/**
+ * _print - Prints an integer.
+ * @n: The integer to print.
+ */
+void _print(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		_print(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-
+int _atoi(char *s);
+void _print(int n);
+void _puts(char *str);
+void _putchar(char c);
+void _exit(int status);
 
 #endif
