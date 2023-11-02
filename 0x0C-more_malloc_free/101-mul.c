846b577b6fcf59dbf07d5ea3b4d3bd59bb1ecefc
medriadi <mohamedriadi@outlook.com>
2023-11-02 17:20:17 +0100

    Nvm we roll back

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,94 +1,29 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
-* is_positive_number - Checks if a string is a positive number.
-* @str: The string to check.
-* Return: 1 if it's a positive number, 0 otherwise.
-*/
-int is_positive_number(const char *str)
-{
-	if (str == NULL || *str == '\0')
-		return (0);
-
-	while (*str)
-	{
-		if (!isdigit(*str))
-			return (0);
-		str++;
-	}
-
-	return (1);
-}
-
-/**
-* multiply_strings - Multiplies two positive numbers represented as strings.
-* @num1: The first number as a string.
-* @num2: The second number as a string.
-* Return: A dynamically allocated string representing the result.
-*/
-char *multiply_strings(const char *num1, const char *num2)
-{
-	int len1 = strlen(num1);
-
-	int len2 = strlen(num2);
-
-	int len_result = len1 + len2;
-
-	int *result = calloc(len_result, sizeof(int));
-
-	for (int i = len1 - 1; i >= 0; i--)
-
-	{
-		for (int j = len2 - 1; j >= 0; j--)
-
-		{
-			int mul = (num1[i] - '0') * (num2[j] - '0');
-
-			int sum = mul + result[i + j + 1];
-
-			result[i + j + 1] = sum % 10;
-			result[i + j] += sum / 10;
-		}
-	}
-
-	int start = 0;
-
-	while (start < len_result - 1 && result[start] == 0)
-		start++;
-
-	char *result_str = malloc(len_result - start + 1);
-
-	for (int i = 0; i < len_result - start; i++)
-
-		result_str[i] = result[i + start] + '0';
-	result_str[len_result - start] = '\0';
-
-	free(result);
-
-	return (result_str);
-}
-
-/**
- * main - Entry point of the program to multiply two positive numbers.
+ * main - Multiplies two positive numbers.
  * @argc: The number of command-line arguments.
- * @argv: An array of strings representing the command-line arguments.
- *
- * Return: 0 on success, 98 on incorrect arguments or non-positive numbers.
+ * @argv: An array of strings representing the arguments.
+ * Return: 0 for success, 98 for incorrect argument count.
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !is_positive_number(argv[1]) || !is_positive_number(argv[2]))
+	int n1, n2, mul;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
 	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		mul = n1 * n2;
 
-	char *result = multiply_strings(argv[1], argv[2]);
-
-	printf("%s\n", result);
-	free(result);
+		printf("%d\n", mul);
+	}
 
 	return (0);
 }
