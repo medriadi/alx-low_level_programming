31c2c07707fa099eed480485c38443a014addd88
medriadi <mohamedriadi@outlook.com>
2023-11-08 17:03:25 +0100

    Task 3 FIX FIX FIX

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,8 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 /**
 * get_op_func - selects the correct function to perform the operation
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,19 +10,29 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+	char *operator;
+	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	int num1 = atoi(argv[1]);
-	char *operator = argv[2];
-	int num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
 
-	int (*func)(int, int) = get_op_func(operator);
+	func = get_op_func(operator);
 
-	int result = func(num1, num2);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	result = func(num1, num2);
 
 	printf("%d\n", result);
 
