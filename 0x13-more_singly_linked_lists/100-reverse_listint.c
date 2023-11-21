0304096a4186a577578e4acaf040c176cb19a418
medriadi <mohamedriadi@outlook.com>
2023-11-21 16:51:10 +0100

    Task 11 FIX

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,18 +8,18 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *next_node = NULL;
+	listint_t *prev_node = NULL;
 
-	while (current != NULL)
+	if (head == NULL)
+		return (NULL);
+	while (*head != NULL)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
 	}
-
-	*head = prev;
+	*head = prev_node;
 	return (*head);
 }
