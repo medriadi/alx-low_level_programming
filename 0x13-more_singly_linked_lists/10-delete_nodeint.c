6b14b7bcea7a0a04e4d7cb422733673574f5d269
medriadi <mohamedriadi@outlook.com>
2023-11-21 14:50:59 +0100

    Task 10 FIX

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,53 +15,35 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0, list_len = listint_len(*head);
+	listint_t *node, *prev_node;
+	unsigned int i = 0;
 
-	if (index < 0 || index >= list_len)
-	return (-1);
-	while (head != NULL)
+	if (!*head || !head)
+		return (-1);
+
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+
+	node = *head;
+
+	while (node)
 	{
 		if (i == index)
 		{
-			old_node = *head;
-			if (i == 0)
-			{
-				*head = old_node->next;
-				free(old_node);
-				return (1);
-			}
-			previous_node->next = old_node->next;
-			free(old_node);
+			prev_node->next = node->next;
+			free(node);
 			return (1);
 		}
-		else if ((i + 1) == index)
-		previous_node = *head;
-		head = &((*head)->next);
+
 		i++;
+		prev_node = node;
+		node = node->next;
 	}
+
 	return (-1);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Description: This function traverses the linked list starting from the head,
- * counting the nodes until it reaches the end of the list.
- *
- * Return: the number of nodes in the list
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,22 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
-* struct listint_s - singly linked list
-* @n: integer data
-* @next: pointer to the next node in the list
-*
-* Description: This struct represents a node in a singly linked list.
-* Each node contains an integer data and a pointer to the next node.
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
 typedef struct listint_s
 {
-int n;
-
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
