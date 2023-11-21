9ebba0f67879d67ba3692cef940c8d6d7c245f51
medriadi <mohamedriadi@outlook.com>
2023-11-21 17:02:08 +0100

    Roll it back this version was better at least

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,6 +35,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size);
 
 #endif
