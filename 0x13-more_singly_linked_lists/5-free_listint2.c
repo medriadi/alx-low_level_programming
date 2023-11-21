1787e6c760e5bfebc63eb41f2294b2b127236bf9
medriadi <mohamedriadi@outlook.com>
2023-11-21 14:03:23 +0100

    Task 5 FiX

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,10 +4,10 @@
  * free_listint2 - Frees a linked list
  * @head: Pointer to the head of the list
  *
- * This function traverses the linked list starting from the head, freeing
- * each node along the way. It does this by temporarily storing the current
- * node in a variable (cursor), freeing the current node, and then advancing
- * the head pointer to the next node. The function continues until it
+ * Description: This function traverses the linked list starting from the head,
+ * freeing each node along the way. It does this by temporarily storing the
+ * current node in a variable (cursor), advancing the head pointer to the next
+ * node, and then freeing the current node. The function continues until it
  * reaches the end of the list (indicated by a NULL head pointer). After
  * freeing all nodes, it sets the head pointer to NULL.
  *
@@ -16,17 +16,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *cursor;
-	listint_t **temp = head;
 
-	if (temp != NULL)
+	if (head != NULL && *head != NULL)
 	{
 		while (*head != NULL)
 		{
 			cursor = *head;
-			free(cursor);
 			*head = (*head)->next;
+			free(cursor);
 		}
 
-		*temp = NULL;
+	  *head = NULL;
 	}
 }
