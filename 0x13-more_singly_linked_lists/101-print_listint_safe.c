2c1d17ca787911a2aef6a4fe3aaa459be0840a2a
medriadi <mohamedriadi@outlook.com>
2023-11-22 14:20:08 +0100

    forget about it

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,44 +1,21 @@
 #include "lists.h"
 
 /**
-* print_listint_safe - prints a linked list
-* @head: pointer to the head of the list
-*
-* Return: the number of nodes in the list
-*/
+ * print_listint_safe - prints a linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
-	struct listint_t *visited[100];
-	size_t visited_count = 0;
 
 	while (current != NULL)
 	{
-		bool is_visited = false;
-
-		for (size_t i = 0; i < visited_count; i++)
-		{
-			if (visited[i] == current)
-			{
-				is_visited = true;
-				break;
-			}
-		}
-
-		if (!is_visited)
-		{
-			printf("[%p] %d\n", (void *)current, current->n);
-			visited[visited_count++] = (struct listint_t *)current;
-			count++;
-		}
-		else
-		{
-			printf("Loop detected at node %p\n", (void *)current);
-			break;
-		}
-
+		printf("[%p] %d\n", (void *)current, current->n);
 		current = current->next;
+		count++;
 	}
 
 	return (count);
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 /**
  * struct listint_s - singly linked list
